8c691d28df232d7220768b4f3ae55426a6e6e823
Kazbek T. <kazibo@gmail.com>
2025-03-17 16:04:29 +0300

    HW_9.02 - удалены StreamStorage классы, serializer в отдельной папке

--- src/com/urise/webapp/storage/FileStorage.java
+++ src/com/urise/webapp/storage/FileStorage.java
@@ -1,27 +1,119 @@
 package com.urise.webapp.storage;
 
+import com.urise.webapp.exception.StorageException;
 import com.urise.webapp.model.Resume;
+import com.urise.webapp.storage.serializer.ObjectStreamSerializer;
+import com.urise.webapp.storage.serializer.StreamSerializer;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
-public class FileStorage extends AbstractFileStorage {
+public class FileStorage extends AbstractStorage<File> {
     private StreamSerializer serializer;
+    private final File storage;
 
     protected FileStorage(File storage) {
-        super(storage);
+        Objects.requireNonNull(storage, "directory must not be null");
+
+        if (!storage.isDirectory()) {
+            throw new IllegalArgumentException(storage + " is not a directory");
+        }
+
+        if (!storage.canRead() || !storage.canWrite()) {
+            throw new IllegalArgumentException("No read/write access to directory: " + storage);
+        }
+        this.storage = storage;
         serializer = new ObjectStreamSerializer();
     }
 
-    @Override
-    protected void doWrite(Resume resume, OutputStream file) throws IOException {
-        serializer.doWrite(resume, file);
+    protected void doWrite(Resume resume, OutputStream os) throws IOException {
+        serializer.doWrite(resume, os);
+    }
+
+    protected Resume doRead(InputStream is) throws IOException {
+        return serializer.doRead(is);
     }
 
     @Override
-    protected Resume doRead(InputStream is) throws IOException {
-        return serializer.doRead(is);
+    protected File getSearchKey(String uuid) {
+        return new File(storage, uuid);
+    }
+
+    @Override
+    protected void doUpdate(Resume r, File searchKey) {
+        try {
+            doWrite(r, new BufferedOutputStream(new FileOutputStream(searchKey)));
+        } catch (IOException e) {
+            throw new StorageException("Failed to write resume", r.getUuid(), e);
+        }
+    }
+
+    @Override
+    protected boolean isExist(File searchKey) {
+        return searchKey.exists();
+    }
+
+    @Override
+    protected void doSave(Resume r, File file) {
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            throw new StorageException("Filed to create file" + file.getAbsolutePath(), file.getName(), e);
+        }
+        doUpdate(r, file);
+    }
+
+    @Override
+    protected Resume doGet(File searchKey) {
+        try {
+            return doRead(new BufferedInputStream(new FileInputStream(searchKey)));
+        } catch (IOException e) {
+            throw new StorageException("Failed to read resume", searchKey.getName(), e);
+        }
+    }
+
+    @Override
+    protected void doDelete(File searchKey) {
+        if (!searchKey.delete()) {
+            throw new StorageException("Failed to delete resume", searchKey.getName());
+        }
+    }
+
+    @Override
+    protected List<Resume> doCopyAll() {
+        File[] files = storage.listFiles();
+        if (files == null) {
+            throw new StorageException("Failed to copy storage", storage.getName());
+        } else {
+            List<Resume> storageCopy = new ArrayList<>();
+            for (File file : files) {
+                storageCopy.add(doGet(file));
+            }
+            return storageCopy;
+        }
+    }
+
+    @Override
+    public void clear() {
+        File[] files = storage.listFiles();
+        if (files == null) {
+            throw new StorageException("Failed to clear storage", storage.getName());
+        } else {
+            for (File file : files) {
+                doDelete(file);
+            }
+        }
+    }
+
+    @Override
+    public int size() {
+        String[] files = storage.list();
+        if (files == null) {
+            throw new StorageException("Failed to return storage size", storage.getName());
+        } else {
+            return files.length;
+        }
     }
 }
--- src/com/urise/webapp/storage/PathStorage.java
+++ src/com/urise/webapp/storage/PathStorage.java
@@ -1,26 +1,117 @@
 package com.urise.webapp.storage;
 
+import com.urise.webapp.exception.StorageException;
 import com.urise.webapp.model.Resume;
+import com.urise.webapp.storage.serializer.ObjectStreamSerializer;
+import com.urise.webapp.storage.serializer.StreamSerializer;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
-public class PathStorage extends AbstractPathStorage {
+public class PathStorage extends AbstractStorage<Path> {
     private StreamSerializer serializer;
+    private Path storage;
 
     protected PathStorage(String dir) {
-        super(dir);
+        Objects.requireNonNull(dir, "directory must not be null");
+        storage = Paths.get(dir);
+        if (!Files.isDirectory(storage)) {
+            throw new IllegalArgumentException(storage + " is not a directory");
+        }
+        if (!Files.isReadable(storage) || !Files.isWritable(storage)) {
+            throw new IllegalArgumentException("No read/write access to directory: " + storage);
+        }
         serializer = new ObjectStreamSerializer();
     }
 
-    @Override
     protected void doWrite(Resume resume, OutputStream os) throws IOException {
         serializer.doWrite(resume, os);
     }
 
-    @Override
     protected Resume doRead(InputStream is) throws IOException {
         return serializer.doRead(is);
+    }
+
+    @Override
+    protected Path getSearchKey(String uuid) {
+        return storage.resolve(uuid);
+    }
+
+    @Override
+    protected void doUpdate(Resume r, Path searchKey) {
+        try {
+            doWrite(r, new BufferedOutputStream(Files.newOutputStream(searchKey)));
+        } catch (IOException e) {
+            throw new StorageException("Failed to write resume", r.getUuid(), e);
+        }
+    }
+
+    @Override
+    protected boolean isExist(Path searchKey) {
+        return Files.exists(searchKey);
+    }
+
+    @Override
+    protected void doSave(Resume r, Path searchKey) {
+        doUpdate(r, searchKey);
+    }
+
+    @Override
+    protected Resume doGet(Path searchKey) {
+        try {
+            if (Files.size(searchKey) == 0) {
+                throw new StorageException("File is empty", searchKey.getFileName().toString());
+            }
+            return doRead(new BufferedInputStream(Files.newInputStream(searchKey)));
+        } catch (IOException e) {
+            throw new StorageException("Failed to read resume", searchKey.getFileName().toString(), e);
+        }
+    }
+
+    @Override
+    protected void doDelete(Path searchKey) {
+        try {
+            Files.delete(searchKey);
+        } catch (IOException e) {
+            throw new StorageException("Failed to delete resume", searchKey.getFileName().toString(), e);
+        }
+    }
+
+    @Override
+    protected List<Resume> doCopyAll() {
+        List<Resume> list = new ArrayList<>();
+
+        try (DirectoryStream<Path> files = Files.newDirectoryStream(storage)) {
+            for (Path path : files) {
+                list.add(doGet(path));
+            }
+        } catch (IOException e) {
+            throw new StorageException("Failed to copy storage", storage.getFileName().toString());
+        }
+        return list;
+    }
+
+    @Override
+    public void clear() {
+        try {
+            Files.list(storage).forEach(this::doDelete);
+        } catch (IOException e) {
+            throw new StorageException("Failed to clear storage", storage.getFileName().toString());
+        }
+    }
+
+    @Override
+    public int size() {
+        try {
+            return (int) Files.list(storage).count();
+        } catch (IOException e) {
+            throw new StorageException("Failed to return storage size", storage.getFileName().toString());
+        }
     }
 }
