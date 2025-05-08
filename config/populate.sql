2e97ce5d48fd07a13d1aae143225e5e4b82e5b41
Kazbek <kazibo@gmail.com>
2025-05-08 18:03:32 +0300

    HW_15.6 HTML servlet table

--- config/populate.sql
+++ config/populate.sql
@@ -1,15 +1,28 @@
 INSERT INTO resume (uuid, full_name)
 VALUES ('7de882da-02f2-4d16-8daa-60660aaf4071', 'Name1'),
-       ('a97b3ac3-3817-4c3f-8a5f-178497311f1d', 'Name2'),
-        ('dd0a70d1-5ed3-479a-b452-d5e04f21ca73', 'Name3');
+       ('a97b3ac3-3817-4c3f-8a5f-178497311f1d', 'Name2');
 
 INSERT INTO contact (type, value, resume_uuid)
 VALUES ('SKYPE', 'skypeg', '7de882da-02f2-4d16-8daa-60660aaf4071'),
-       ('PHONE', '434343534', 'a97b3ac3-3817-4c3f-8a5f-178497311f1d');
+       ('SKYPE', 'skypeg', 'a97b3ac3-3817-4c3f-8a5f-178497311f1d'),
+       ('PHONE', '434343534', '7de882da-02f2-4d16-8daa-60660aaf4071'),
+       ('PHONE', '434343534', 'a97b3ac3-3817-4c3f-8a5f-178497311f1d'),
+       ('EMAIL', 'skypeg', '7de882da-02f2-4d16-8daa-60660aaf4071'),
+       ('EMAIL', 'skypeg', 'a97b3ac3-3817-4c3f-8a5f-178497311f1d'),
+       ('LINKEDIN', '434343534', '7de882da-02f2-4d16-8daa-60660aaf4071'),
+       ('LINKEDIN', '434343534', 'a97b3ac3-3817-4c3f-8a5f-178497311f1d'),
+       ('GITHUB', 'skypeg', '7de882da-02f2-4d16-8daa-60660aaf4071'),
+       ('GITHUB', 'skypeg', 'a97b3ac3-3817-4c3f-8a5f-178497311f1d'),
+       ('STACKOVERFLOW', '434343534', '7de882da-02f2-4d16-8daa-60660aaf4071'),
+       ('STACKOVERFLOW', '434343534', 'a97b3ac3-3817-4c3f-8a5f-178497311f1d'),
+       ('HOMEPAGE', 'skypeg', '7de882da-02f2-4d16-8daa-60660aaf4071'),
+       ('HOMEPAGE', 'skypeg', 'a97b3ac3-3817-4c3f-8a5f-178497311f1d');
 
 INSERT INTO text_section (ts_type, ts_value, ts_resume_uuid)
 VALUES ('PERSONAL', 'Tatatata', '7de882da-02f2-4d16-8daa-60660aaf4071'),
-       ('OBJECTIVE', 'Blabla', 'a97b3ac3-3817-4c3f-8a5f-178497311f1d');
+       ('OBJECTIVE', 'Blabla', 'a97b3ac3-3817-4c3f-8a5f-178497311f1d'),
+       ('PERSONAL', 'Tatatata', 'a97b3ac3-3817-4c3f-8a5f-178497311f1d'),
+       ('OBJECTIVE', 'Blabla', '7de882da-02f2-4d16-8daa-60660aaf4071');
 
 INSERT INTO list_section (ls_type, ls_value, ls_resume_uuid)
 VALUES ('ACHIEVEMENT', 'AchiAchi\nAchAch\nAcAcAc', '7de882da-02f2-4d16-8daa-60660aaf4071'),
--- src/com/urise/webapp/web/ResumeServlet.java
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -2,7 +2,7 @@ package com.urise.webapp.web;
 
 import com.urise.webapp.Config;
 import com.urise.webapp.model.Resume;
-import com.urise.webapp.storage.SqlStorage;
+import com.urise.webapp.model.SectionType;
 import com.urise.webapp.storage.Storage;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -29,15 +29,33 @@ public class ResumeServlet extends HttpServlet {
 
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h2>Resumes</h2>");
-        out.println("<table border='1'>");
-        out.println("<tr><th>UUID</th><th>Full Name</th></tr>");
-        for (Resume r : resumes) {
-            out.printf("<tr><td>%s</td><td>%s</td></tr>%n", r.getUuid(), r.getFullName());
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Resumes</title>");
+        out.println("<style>");
+        out.println("table { border-collapse: collapse; width: 100%; }");
+        out.println("th, td { border: 1px solid #ddd; padding: 8px; text-align: left; }");
+        out.println("th { background-color: #f2f2f2; }");
+        out.println("</style>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Resume List</h1>");
+        out.println("<table>");
+        out.println("<tr><th>ID</th><th>Full Name</th><th>Email</th><th>Skills</th></tr>");
+
+        for (Resume resume : resumes) {
+            out.println("<tr>");
+            out.println("<td>" + resume.getUuid() + "</td>");
+            out.println("<td>" + resume.getFullName() + "</td>");
+            out.println("<td>" + resume.getContact(Resume.ContactType.SKYPE) + "</td>");
+            out.println("<td>" + resume.getSection(SectionType.OBJECTIVE) + "</td>");
+            out.println("</tr>");
         }
+
         out.println("</table>");
-        out.println("</body></html>");
+        out.println("</body>");
+        out.println("</html>");
     }
 
     @Override
